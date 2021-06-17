1962d0bd938a722b341081c3bbc3ef799e6932f3
Execionere <54672824+Execionere@users.noreply.github.com>
2021-06-17 12:15:25 +0300

    Initial commit

---
+++ src/WhileTest.java
@@ -0,0 +1,11 @@
+public class WhileTest {
+    public static void main(String[] args) {
+        int x = 0;
+        String a = "JAVA ";
+
+        while (x < 10) {
+            System.out.print(a);
+            x = x + 1;
+        }
+    }
+}
