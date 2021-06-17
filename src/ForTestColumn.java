1f4be8f326ae2ae4397654914e72efdecb27a3b2
Execionere <54672824+Execionere@users.noreply.github.com>
2021-06-17 13:08:40 +0300

    rdy

---
+++ src/ForTestColumn.java
@@ -0,0 +1,8 @@
+public class ForTestColumn {
+    public static void main(String[] args) {
+        String a = "JAVA ";
+        for (int x = 0; x<=9; x++) {
+            System.out.println(a);
+        }
+    }
+}
