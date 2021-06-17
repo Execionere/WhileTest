d69daabadf83264068a998b315faade8c769a6a0
Execionere <54672824+Execionere@users.noreply.github.com>
2021-06-17 15:34:02 +0300

    rdy

---
+++ src/ForMultiplication.java
@@ -0,0 +1,8 @@
+public class ForMultiplication {
+    public static void main(String[] args) {
+        int k = 9;
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + " x " + k + " = " + i * k);
+        }
+    }
+}
