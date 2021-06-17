58d35d83d7c870f06dd605096e30eaab4b08e1b4
Execionere <54672824+Execionere@users.noreply.github.com>
2021-06-17 13:26:53 +0300

    rdy

---
+++ src/ForOlympiadYear.java
@@ -0,0 +1,7 @@
+public class ForOlympiadYear {
+    public static void main(String[] args) {
+        for (int year = 1980; year<=2020; year = year + 4){
+            System.out.println("Олимпиада " + year + " года");
+        }
+    }
+}
---
+++ src/WhileOlympiadYear.java
@@ -0,0 +1,9 @@
+public class WhileOlympiadYear {
+    public static void main(String[] args) {
+        int year = 1980;
+        while (year <= 2020) {
+            System.out.println("Олимпиада " + year + " года");
+            year = year + 4;
+        }
+    }
+}
